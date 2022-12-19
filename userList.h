38e511e03af77518ef0c1d3efd8be8b958a0f1a1
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2022-12-19 20:48:31 +0800

    V1
    Pembuatan ADT untuk List User

--- userList.h
+++ userList.h
@@ -1,6 +1,30 @@
 #ifndef USERLIST_H_INCLUDED
 #define USERLIST_H_INCLUDED
 
+struct userData {
+   string username,password;
+   int userID;
+} ;
 
+typedef struct elmUser *adr_UL;
+typedef struct userData infotype;
+typedef struct elmProduct *adr_PL;
+
+struct ElmUser{
+    infotype info;
+    adr_UL next;
+    adr_PL wishList;
+};
+
+struct ListUser {
+    adr_UL first;
+};
+
+void createListUser(ListUser &L);
+adr_UL createElemenUserList(infotype X);
+void insertUserFirst( ListUser &L, adr_UL &P);
+void showUsers(ListUser L);
+void deleteUser(ListUser &L, adr_UL &P);
+adr_UL findUser(ListUser L, string username, int userID);
 
 #endif // USERLIST_H_INCLUDED
