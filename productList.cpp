d5c019222a1b5fae47213cbb94fa4c5f2708522f
M Isa Al Anshori <misaalanshori@student.telkomuniversity.ac.id>
2022-12-26 16:32:14 +0700

    create productList

--- productList.cpp
+++ productList.cpp
@@ -1,1 +1,63 @@
+#include "header.h"
 #include "productList.h"
+
+void createListProduct(ListProduct &L) {
+    first(L) = NULL;
+}
+
+adr_PL createElemenProductList(infotypeP X) {
+    adr_PL produk = new elmProduct;
+    info(produk) = X;
+    next(produk) = NULL;
+    return produk;
+
+}
+
+void insertProductLast(ListProduct &L, adr_PL P) { 
+    adr_PL elm = first(L);
+    if (elm == NULL) {
+        first(L) = P;
+    } else {
+        while (next(elm) != NULL) {
+            elm = next(elm);
+        }
+        next(elm) = P;
+    }
+    
+}
+
+void showProducts(ListProduct L) {
+    adr_PL elm = first(L);
+    cout << "Daftar Produk:" << endl; 
+    if (elm == NULL) {
+        cout << "Produk Kosong!" << endl;
+    } else {
+        while (elm != NULL) {
+            elm = next(elm);
+            cout << info(elm).productID << ". " << info(elm).nama << " (Rp" << info(elm).harga << ")" << endl;
+        }
+    }
+}
+
+
+void deleteProduct(ListProduct &L, adr_PL &P, int productID){
+    adr_PL a = first(L);
+    adr_PL prec = first(L);
+
+    while  (next(a) != NULL && info(next(a)).productID != productID){
+        prec = a;
+        a = next(a);
+    }
+    P = next(prec);
+    next(prec) = next(P);
+    next(P) = NULL;
+};
+
+
+adr_PL findProduct(ListProduct L, int productID) {
+    adr_PL a = first(L);
+    while (a != NULL && info(a).productID != productID) {
+        a = next(a);
+    }
+    return a;
+}
--- productList.h
+++ productList.h
@@ -1,6 +1,30 @@
 #ifndef PRODUCTLIST_H_INCLUDED
 #define PRODUCTLIST_H_INCLUDED
 
+struct productData {
+    int productID;
+    string nama;
+    int harga;
+} ;
 
+typedef struct productData infotypeP;
+typedef struct elmProduct *adr_PL;
+
+struct elmProduct{
+    infotypeP info;
+    adr_PL next;
+};
+
+struct ListProduct {
+    adr_PL first;
+};
+
+void createListProduct(ListProduct &L);
+adr_PL createElemenProductList(infotypeP X);
+void insertProductLast( ListProduct &L, adr_PL &P);
+void showProducts(ListProduct L);
+void deleteProductAfter(ListProduct &L, adr_PL Prec, adr_PL &P);
+void deleteProduct(ListProduct &L, int productID);
+adr_PL findProduct(ListProduct L, int productID);
 
 #endif // PRODUCTLIST_H_INCLUDED
